a5e67667a0afc3b1b18c756c6f6a74badd2636ec
IvanE <ivan.espinoza.mora@est.una.ac.cr>
2026-05-22 16:25:26 -0600

    Render conection

--- cletaeats_backend/data/database/db_connection.py
+++ cletaeats_backend/data/database/db_connection.py
@@ -4,9 +4,12 @@ from sqlalchemy import create_engine
 from sqlalchemy import MetaData
 
 _BASE_DIR = os.path.normpath(
-    os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", "..", "..")
+    os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", "..")
 )
-DB_PATH = os.path.join(_BASE_DIR, "cletaeats_data", "cletaeats.db")
+DB_PATH = os.environ.get(
+    "DB_PATH",
+    os.path.join(_BASE_DIR, "cletaeats_data", "cletaeats.db")
+)
 
 engine = create_engine(
     f"sqlite:///{DB_PATH}",
